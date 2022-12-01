1f891264a1bdd914870bb1cdb3904a7563d8468e
ZBelouchi <zachary.belouchi@gmail.com>
2022-12-01 14:52:50 -0500

    created images.js

---
+++ src/assets/images.js
@@ -0,0 +1,23 @@
+import cloud from '../assets/weather-icon-layers/cloud.png'
+import darkcloud from "../assets/weather-icon-layers/darkcloud.png"
+import fog from "../assets/weather-icon-layers/fog.png"
+import hail from "../assets/weather-icon-layers/hail.png"
+import lightning from "../assets/weather-icon-layers/lightning.png"
+import moon from "../assets/weather-icon-layers/moon.png"
+import rain from "../assets/weather-icon-layers/rain.png"
+import snow from "../assets/weather-icon-layers/snow.png"
+import sun from "../assets/weather-icon-layers/sun.png"
+
+const IMAGES = {
+    cloud: cloud,
+    darkcloud: darkcloud,
+    fog: fog,
+    hail: hail,
+    lightning: lightning,
+    moon: moon,
+    rain: rain,
+    snow: snow,
+    sun: sun
+}
+
+export default IMAGES
