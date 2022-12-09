ae76b5c496a8fe5a981f6916b24f6ce8196e12f9
ZBelouchi <zachary.belouchi@gmail.com>
2022-12-09 14:02:53 -0500

    created basic functional weather component

--- src/components/LocationSelect.jsx
+++ src/components/LocationSelect.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function LocationSelect() {
+    const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${search}&count=100`, {method: 'GET'})
+            .then(response => response.json())
+            .then(json => {
+                setResults(json.results || [])
+            })
+            .catch(err => console.log(err))
+    }, [search])
+
     return (
         <div className="location-select">
-            <form action=""></form>
+            <label>Search</label>
+            <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
+
+            <div className="search__list">
+                {
+                    results.map((result) => (
+                        <div 
+                            className='search__item'
+                            onClick={() => {navigate("/home", {state: {
+                                lat: result.latitude, 
+                                lon: result.longitude,
+                                primary: result.name,
+                                secondary: result.admin1,
+                                country: result.country
+                            }})}}
+                        >
+                            <p>{result.name}</p>
+                            <p>{result.admin3}</p>
+                            <p>{result.admin1}</p>
+                            <p>{result.country}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,13 +2,18 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import useFetch from '../hooks/useFetch'
 import useDatetime from '../hooks/useDatetime'
+import { useLocation } from 'react-router-dom'
 
 // import Image from './Image'
 import IMAGES from '../assets/images'
 
-export default function Weather() {
+function WeatherOLD() {
     const [weatherData, setWeatherData] = useState({
-        location: "",
+        location: {
+            lat: "",
+            lon: "",
+            name: ""
+        },
         temperature: {
             current: 0,
             high: 0,
@@ -26,26 +31,25 @@ export default function Weather() {
             timeIcon: () => {}
         }
     })
-
-    const [datetime, datetimeMs, datetimeString]  = useDatetime()
-
-
-    // temp data for API
-    const apiPath = `https://api.open-meteo.com/v1/forecast?latitude=39.34&longitude=-84.40&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch&timezone=America%2FNew_York`
-    const lat = 39.34
-    const lon = -84.40
-    const loc = "Monroe"
-    const date = "2022-11-23"
-
-    // useEffect to prevent infinite loop of api calls
+    
+    
+    
+    
     useEffect(() => {
-        fetch(apiPath, {method: 'GET'})
+        fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${location.state.lat.toString()}&longitude=${location.state.lon.toString()}&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch&timezone=America%2FNew_York`,
+            {method: 'GET'}
+        )
         .then(response => response.json())
         .then(json => {
             const twentyFourTemp = json.hourly.temperature_2m.slice(0, 24)
-
+            
             setWeatherData({
-                location: loc,
+                location: {
+                    lat: location.state.lat,
+                    lon: location.state.lon,
+                    name: `${location.state.name}, ${location.state.area}`
+                },
                 temperature: {
                     current: json.current_weather.temperature,
                     high: Math.max(...twentyFourTemp),
@@ -73,39 +77,123 @@ export default function Weather() {
             })
         })
         .catch(err => console.log(err))
-    }, [lat, lon])
-    
-    
-    
-    
+    }, [datetime])
+}
+
+export default function Weather() {
+    const locationState = useLocation()
+    const [datetime, datetimeMs, datetimeString, datetimeOffset]  = useDatetime()
+    const [weatherData, setWeatherData] = useState()
+
+    let args = [
+        `latitude=${locationState.state.lat.toString()}`,
+        `longitude=${locationState.state.lon.toString()}`,
+        `hourly=temperature_2m,weathercode`,
+        `daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset`,
+        `current_weather=true`,
+        `timezone=auto`,
+        `temperature_unit=fahrenheit`,
+        `windspeed_unit=mph`,
+        `precipitation_unit=inch`,
+        `timezone=auto`,
+    ]
+    let apiPath = "https://api.open-meteo.com/v1/forecast?" + args.join("&")
+
+    useEffect(() => {
+        fetch(apiPath, {method: 'GET'})
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+
+                let sunrise = new Date(json.daily.sunrise[0])
+                let sunset = new Date(json.daily.sunset[0])
+                // TODO get accurate local time, not just hourly approximation
+                let local = new Date(Date.parse(json.current_weather.time))
+
+                let hourlyTemps = json.hourly.temperature_2m.map((temp, index) => {
+                // parse data and make list of all hours with time and temp
+                    return [new Date(json.hourly.time[index]), temp]
+                }).filter((item) => {
+                // filter out times that aren't for current day
+                    return item[0].toDateString() === local.toDateString()
+                }).map((item) => {
+                // reduce items to only temp now that time isn't needed
+                    return item[1]
+                })
+
+                setWeatherData({
+                    location: {
+                        primary: locationState.state.primary,
+                        secondary: locationState.state.secondary,
+                        country: locationState.state.country
+                    },
+                    condition: json.current_weather.weathercode,
+                    temperature: {
+                        current: json.current_weather.temperature,
+                        high: Math.max(...hourlyTemps),
+                        low: Math.min(...hourlyTemps)
+                    },
+                    wind: json.current_weather.windspeed,
+                    sunrise: sunrise,
+                    sunset: sunset,
+                    isNighttime: local < sunrise,
+                    localTime: local
+                })
+            })
+            .catch(err => console.log(err))
+    }, [datetime])
+
+    // temporary fix to prevent it from trying to render with undefined data
+    // works but I should find a more elegant solution later
+    try {
+        weatherData.location
+    } catch {
+        return
+    }
 
     return (
         <div className="weather">
-            {/* location */}
-            <p>Location: {weatherData.location}</p>
-            <br/>
-
-            {/* temperature */}
-            <p>Temp: {weatherData.temperature.current}</p>
-            {/* temp high/low */}
-            <p>High/Low: {weatherData.temperature.high} / {weatherData.temperature.low}</p>
-            <br/>
-
-            {/* date/time */}
-            <p>Current Time: {datetimeString}</p>
-            <p>Last Updated: {weatherData.time.lastUpdated}</p>
-            <br/>
-
-            <Condition code={weatherData.condition} time={weatherData.time.timeIcon()}/>
-            <br/>
-            
-            {/* Wind Speed */}
-            <p>Wind: {weatherData.wind.speed} {weatherData.wind.direction}</p>
+            <WeatherBox 
+                location={weatherData.location} 
+                condition={weatherData.condition} 
+                isNighttime={weatherData.isNighttime}
+                temperature={weatherData.temperature} 
+                wind={weatherData.wind}
+            />
         </div>
     )
 }
 
-function Condition({code, time}) {
+function WeatherBox({location, condition, isNighttime, temperature, wind}) {
+    
+    return (
+        <div className="weather__box">
+            <div className="weather__stat weather__stat--location">
+                <h2>{location.primary}</h2>
+                <p>{location.secondary}, {location.country}</p>
+            </div>
+            <div className="row">
+                <div className="">
+                    <div className="weather__stat weather__stat--condition">
+                        <Condition code={condition} isNighttime={isNighttime}/>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="weather__stat weather__stat--temperature">
+                        <p>{temperature.current} °F</p>
+                        <p>{temperature.high} / {temperature.low}</p>
+                    </div>
+                    <div className="weather__stat weather__stat--wind">
+                        <p>{wind}mph</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+function Condition({code, isNighttime}) {
     const CONDITIONS = {
         0: {
             description: "Clear Sky",
@@ -220,17 +308,44 @@ function Condition({code, time}) {
             iconLayers: [IMAGES.darkcloud, IMAGES.lightning, IMAGES.hail]
         },
     }
-    
+
     return (
-        <div className="weather__condition">
-            {/* Weather */}
-            <p>Weather: {CONDITIONS[code].description || `Invalid Weather Code '${code}'`}</p>
+        <>
             <div className="weather__icon">
-                <img src={time} alt="" className='weather__icon--layer'/>
+                <img src={isNighttime ? IMAGES.moon : IMAGES.sun} alt="" className='weather__icon--layer'/>
                 {
                     CONDITIONS[code].iconLayers.map((layer) => {return <img src={layer} alt="weather-icon-layer"  className='weather__icon--layer'/>})
                 }
             </div>
-        </div>
+            <p>{CONDITIONS[code].description || `Invalid Weather Code '${code}'`}</p>
+        </>
     )
 }
+
+
+
+
+
+
+/* location */
+{/* <p>Location: {weatherData.location.name}</p>
+<p>Latitude: {weatherData.location.lat}</p>
+<p>Longitude: {weatherData.location.lon}</p>
+<br/> */}
+
+/* temperature */
+{/* <p>Temp: {weatherData.temperature.current}</p> */}
+/* temp high/low */
+{/* <p>High/Low: {weatherData.temperature.high} / {weatherData.temperature.low}</p>
+<br/> */}
+
+/* date/time */
+{/* <p>Current Time: {datetimeString}</p> */}
+/* <p>Last Updated: {weatherData.time.lastUpdated}</p> */
+{/* <br/> */}
+
+{/* <Condition code={weatherData.condition} time={weatherData.time.timeIcon()}/> */}
+{/* <br/> */}
+
+/* Wind Speed */
+{/* <p>Wind: {weatherData.wind.speed} {weatherData.wind.direction}</p> */}
